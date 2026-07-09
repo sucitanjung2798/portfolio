f6c5b36e089036fb59a72e42ea9ad78734279e6d
sucitanjung2798 <sucitanjung2798@gmail.com>
2026-07-09 09:10:42 +0700

    chore: setup project architecture

---
+++ src/constants/navigation.ts
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Projects",
+    href: "/projects",
+  },
+  {
+    label: "Experience",
+    href: "/experience",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
---
+++ src/constants/social.ts
@@ -0,0 +1,5 @@
+export const socialLinks = {
+  github: "",
+  linkedin: "",
+  email: "sucitanjung2701@gmail.com",
+};
